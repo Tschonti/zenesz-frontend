309e478a3d01d7f8f2e90aed687369080dd8ba68
Sámuel Fekete <feketesamu@gmail.com>
2022-01-09 22:01:54 +0100

    playlist fix for real

--- src/reducers/playlistReducer.js
+++ src/reducers/playlistReducer.js
@@ -25,7 +25,7 @@ export default (state = defaultState, action) => {
                 return state
             }
             let curIdx = state.currentIndex
-            if (remIdx <= curIdx) {
+            if (remIdx < curIdx || remIdx === state.list.length - 1) {
                 curIdx -= 1
             }
             const clonedState = [...state.list]
